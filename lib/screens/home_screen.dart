28ee16b83b3d6bed2593ef28d97653239979c9f0
adam <adamlou81@gmail.com>
2023-01-04 20:15:45 +0800

    home test 3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,14 @@ import 'package:flutter/material.dart';
 
 import '../pages/contacts_pages.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final pages = const [
     MessagesPage(),
     NotificationsPage(),
@@ -16,19 +21,27 @@ class HomeScreen extends StatelessWidget {
     ContactsPage(),
   ];
 
+  var index = 0;
   //home git test
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[0],
-      bottomNavigationBar: const _BottomNavigationBar(),
+      body: pages[index],
+      bottomNavigationBar: _BottomNavigationBar(
+        onItemSelected: (i){
+          setState(() {
+            index = i;
+          });
+        },
+      ),
     );
   }
 }
 
 class _BottomNavigationBar extends StatelessWidget {
-  const _BottomNavigationBar({Key? key}) : super(key: key);
+  const _BottomNavigationBar({Key? key, required this.onItemSelected}) : super(key: key);
+
+  final ValueChanged<int> onItemSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -37,22 +50,30 @@ class _BottomNavigationBar extends StatelessWidget {
       bottom: true,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: const [
+        children: [
           NavigationBarItem(
+            index: 0,
             lable: 'message',
             icon: CupertinoIcons.bubble_left_bubble_right_fill,
+            onTap: onItemSelected,
           ),
           NavigationBarItem(
+            index: 1,
             lable: 'message',
             icon: CupertinoIcons.bell_solid,
+            onTap: onItemSelected,
           ),
           NavigationBarItem(
+            index: 2,
             lable: 'message',
             icon: CupertinoIcons.phone_fill,
+            onTap: onItemSelected,
           ),
           NavigationBarItem(
+            index: 3,
             lable: 'message',
             icon: CupertinoIcons.person_2_fill,
+            onTap: onItemSelected,
           ),
         ],
       ),
@@ -61,36 +82,46 @@ class _BottomNavigationBar extends StatelessWidget {
 }
 
 class NavigationBarItem extends StatelessWidget {
-  const NavigationBarItem({
+  NavigationBarItem({
     Key? key,
+    //required this.onTap,
+    required this.index,
     required this.lable,
-    required this.icon
+    required this.icon,
+    required this.onTap,
   }) : super(key: key);
 
+  final int index;
   final String lable;
   final IconData icon;
+  final ValueChanged<int> onTap;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 70,
-        child:
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              icon,
-              size:20,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              lable,
-              style: const TextStyle(fontSize: 11),
-            )
-          ],
-        )
+    return GestureDetector(
+      onTap: (){
+        onTap(index);
+      },
+      child: SizedBox(
+          height: 70,
+          child:
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                icon,
+                size:20,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                lable,
+                style: const TextStyle(fontSize: 11),
+              )
+            ],
+          )
+      ),
     );
 
   }
