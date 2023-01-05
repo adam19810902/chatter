136ad2301773492553174e183fe26621a6fa3f71
adam <adamlou81@gmail.com>
2023-01-05 14:32:22 +0800

    first commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chatter/pages/calls_page.dart';
 import 'package:chatter/pages/messages_page.dart';
 import 'package:chatter/pages/notifications_page.dart';
+import 'package:chatter/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<int> pageIndex = ValueNotifier(0);
+
   final pages = const [
     MessagesPage(),
     NotificationsPage(),
@@ -21,27 +24,44 @@ class _HomeScreenState extends State<HomeScreen> {
     ContactsPage(),
   ];
 
-  var index = 0;
+  //var index = 0;
   //home git test
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[index],
+      body: ValueListenableBuilder(
+        valueListenable: pageIndex,
+        builder: (BuildContext context, int index, _){
+          return pages[index];
+        },
+      ),
       bottomNavigationBar: _BottomNavigationBar(
         onItemSelected: (i){
-          setState(() {
-            index = i;
-          });
+          //print(i);
+          pageIndex.value = i;
         },
       ),
     );
   }
 }
 
-class _BottomNavigationBar extends StatelessWidget {
+class _BottomNavigationBar extends StatefulWidget {
   const _BottomNavigationBar({Key? key, required this.onItemSelected}) : super(key: key);
 
   final ValueChanged<int> onItemSelected;
+
+  @override
+  State<_BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<_BottomNavigationBar> {
+  var selectedIndex = 0;
+  void handleItemSelected(index){
+    setState(() {
+      selectedIndex = index;
+    });
+    widget.onItemSelected(index);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,27 +73,31 @@ class _BottomNavigationBar extends StatelessWidget {
         children: [
           NavigationBarItem(
             index: 0,
-            lable: 'message',
+            lable: 'Messages',
             icon: CupertinoIcons.bubble_left_bubble_right_fill,
-            onTap: onItemSelected,
+            isSelected: (selectedIndex == 0),
+            onTap: handleItemSelected,
           ),
           NavigationBarItem(
             index: 1,
-            lable: 'message',
+            lable: 'Notifications',
             icon: CupertinoIcons.bell_solid,
-            onTap: onItemSelected,
+            isSelected: (selectedIndex == 1),
+            onTap: handleItemSelected,
           ),
           NavigationBarItem(
             index: 2,
-            lable: 'message',
+            lable: 'Calls',
             icon: CupertinoIcons.phone_fill,
-            onTap: onItemSelected,
+            isSelected: (selectedIndex == 2),
+            onTap: handleItemSelected,
           ),
           NavigationBarItem(
             index: 3,
-            lable: 'message',
+            lable: 'Contacts',
             icon: CupertinoIcons.person_2_fill,
-            onTap: onItemSelected,
+            isSelected: (selectedIndex == 3),
+            onTap: handleItemSelected,
           ),
         ],
       ),
@@ -88,17 +112,20 @@ class NavigationBarItem extends StatelessWidget {
     required this.index,
     required this.lable,
     required this.icon,
+    this.isSelected = false,
     required this.onTap,
   }) : super(key: key);
 
   final int index;
   final String lable;
   final IconData icon;
+  final bool isSelected;
   final ValueChanged<int> onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior: HitTestBehavior.opaque,
       onTap: (){
         onTap(index);
       },
@@ -111,13 +138,19 @@ class NavigationBarItem extends StatelessWidget {
               Icon(
                 icon,
                 size:20,
+                color: isSelected ? AppColors.secondary : null,
               ),
               const SizedBox(
                 height: 8,
               ),
               Text(
                 lable,
-                style: const TextStyle(fontSize: 11),
+                style: isSelected
+                    ? const TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.secondary)
+                    : const TextStyle(fontSize: 11)
               )
             ],
           )
