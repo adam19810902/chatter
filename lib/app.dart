2e7d68e6f2e241ddbf16023596f3fbd41fa5504b
adam <adamlou81@gmail.com>
2023-01-10 16:37:10 +0800

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,5 @@
+import 'package:logger/logger.dart';
+
+const streamKey = 'rmmfegun8b32';
+
+var logger = Logger();
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:chatter/screens/screens.dart';
 import 'package:chatter/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
-void main() => runApp(const MyApp());
+import 'app.dart';
+
+void main() {
+  final client = StreamChatClient(streamKey);
+
+  runApp(MyApp(
+    client: client,
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.client});
 
+  final StreamChatClient client;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
-      themeMode: ThemeMode.dark,
-      title: 'Chatter',
-      home:HomeScreen()
-    );
+        theme: AppTheme.light(),
+        darkTheme: AppTheme.dark(),
+        themeMode: ThemeMode.dark,
+        title: 'Chatter',
+        builder: (context, child) {
+          return StreamChatCore(
+              client: client,
+              child: child!);
+        },
+        home: const SelectUserScreen());
   }
 }
